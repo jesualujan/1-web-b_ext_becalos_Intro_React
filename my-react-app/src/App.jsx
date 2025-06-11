a1bac0abb9e2c47db1263164e66205d0dfd7b9dc
jesualujan <lujan.oficial@gmail.com>
2025-06-10 21:45:31 -0600

    🐛fixed bug

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,20 +1,59 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useState, useEffect} from 'react'
 import './App.css'
 
+// Componente App -> Este es el componente principal de la aplicación React (PADRE).
 function App() {
-  return (
-    <>
-     <h1>Vite + React</h1>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      
+   // Lógica de programación (JavaScript). 
+   //* 1) useState: Hook para manejar el estado del componente.
+    const [dogs, setDogs] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
-    </>
+   //* 2) useEffect: Hook para manejar efectos secundarios, como la carga de datos.
+   //* 2.1) useEffect: Carga de datos desde una API.
+    useEffect(() => {
+    // 2.2) Función asíncrona para obtener datos de la API.
+    // 2.3) Se utiliza fetch para realizar una solicitud a la API de perros.
+    // 2.4) Se maneja la respuesta y se actualiza el estado de dogs (try/catch para manejar errores).
+     const getDogs = async () => {
+        try {
+            const response = await fetch('https://api.thedogapi.com/v1/breeds');
+            if(!response.ok){
+                throw new Error(`Error ${response.status}: ${response.statusText}`);
+            }
+            const data = await response.json();
+        // 2.5) Se actualiza el estado de dogs con los datos obtenidos (datos de los perritos).
+         setDogs(data);
+            // ver datos en la consola
+            console.log(data);
+      }
+            catch(error){
+                setError(error.message);
+            }
+            finally{
+                setLoading(false);
+            }
+        }
+        getDogs();
+    }, []);
+
+  // Renderizado condicional basado en el estado de la aplicación.
+  if(loading) { return <p>Loading...</p>}
+  if(error) { return <p>Error: {error.message}</p>}
+
+  return (
+    // Estructura HTML de la aplicación.
+    // Utiliza fragmentos <> para agrupar elementos sin añadir nodos adicionales al DOM.
+   <>
+        <div className="card-container">
+        {dogs.map((race) => (
+          <div key={race.id} className="dogs-card">
+            <h2>{race.name}</h2>
+          <p><strong>Temperament:</strong> {race.temperament ?? "Not Specified"}</p>
+          </div>
+        ))}
+        </div>
+   </>
   )
 }
 
