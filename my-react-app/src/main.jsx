cb40148f659504f4ab645b5e52737d6789264220
jesualujan <lujan.oficial@gmail.com>
2025-06-10 20:21:28 -0600

    ✨componentes

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,6 +1,9 @@
+// This file is the entry point for a React application.
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+// Importing styles and main App component
 import './index.css'
+// Importing the main App component
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
