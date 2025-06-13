f5f3416b405c28f879e23c18985d3d9345289811
jesualujan <lujan.oficial@gmail.com>
2025-06-12 19:01:20 -0600

    🎉 creación del proyecto

---
+++ 01.Rick_and_Morty/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Rick and Morty</h1>
+      <p>Welcome to the Rick and Morty app!</p>
+    </>
+  )
+}
+
+export default App
