3d8f033bbe9186308c6766948fffed322cdb741d
jesualujan <lujan.oficial@gmail.com>
2025-06-12 20:19:06 -0600

    ✨ llamada a la API

--- 01.Rick_and_Morty/src/App.jsx
+++ 01.Rick_and_Morty/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
      const [search, setSearch] = useState(''); // Estado para gestionar la búsqueda de personajes
      const [species, setSpecies] = useState(''); //Filtro por especie del personaje
      const [status, setStatus] = useState(''); // Filtro por estado del personaje
-     const [darMode, setDarkMode] = useState(false); // Alterna entre modo claro y oscuro
+     const [darkMode, setDarkMode] = useState(false); // Alterna entre modo claro y oscuro
 
      // useEffect ejecuta su lógica cada vez que el componente cambia o se monta 
      useEffect(() => {
@@ -30,8 +30,34 @@ function App() {
         //Cambia la clase del <body> para activar el modo oscuro o claro
         document.body.className = darkMode ? 'bg-dark text-light' : 'bg-light text-dark';
       }, [darkMode]) // se ejecuta cada vez que cambia el estado de darkMode
+ 
+      // Función asíncrona para obtener los personajes de la API
+      const getCharacters = async (url) => {
+        try{
+          setLoading(true); // Activamos el estado de carga
+          // Establecer un temporizador para simular una carga
+          const timer = new Promise((res) => setTimeout(res, 2000));
+          const fetchPromise = fetch(url); // Realizamos la petición a la API
 
-
+          // Ejecutamos ambas promesas simultáneamente
+          const [response] = await Promise.all([
+            fetchPromise.then(res => {
+              if(!res.ok) throw new Error('Error al obtener los personajes'); //validamos la respuesta
+              return res.json(); // Convertimos la respuesta a JSON
+            }),
+            timer // simulación de retardo
+          ])
+          // Guardamos los datos obtenidos en los estados correspondientes
+          setCharacterts(response.results); // Guardamos los personajes
+          setInfo(response.info); // Guardamos la información de paginación
+        }catch(error){
+          console.error('Ups! Algo salió mal:', error); // Manejamos errores de la petición
+          setCharacters([]); // En caso de error, vaciamos la lista de personajes
+        }
+        finally{
+          setLoading(false); // Desactivamos el estado de carga
+        }
+      }
 
 
   return (
@@ -39,6 +65,7 @@ function App() {
      <Navbar />
       <h1>Rick and Morty</h1>
       <p>Welcome to the Rick and Morty app!</p>
+      
     </>
   )
 }
