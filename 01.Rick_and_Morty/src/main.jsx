2ce3a3feb861c6c0a7839bba8425cd3dd69f63da
jesualujan <lujan.oficial@gmail.com>
2025-06-12 19:21:38 -0600

    ✨ agregamos comentarios al código

--- 01.Rick_and_Morty/src/main.jsx
+++ 01.Rick_and_Morty/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-
+// Importación de módulos de NODEJS para manejar React
+import { StrictMode } from 'react' // Activar verificaciones adicionales en el código para ayudar a detectar problemas.
+import { createRoot } from 'react-dom/client'// Nueva Api de React v.18 para renderizar la aplicación más eficientemente.
+// Importación del componente principal de la aplicación.
 import App from './App.jsx'
 
+// Creación de la raíz del DOM y renderización del componente principal dentro de StrictMode.
+// StrictMode es una herramienta de desarrollo que no afecta la producción, pero ayuda a identificar problemas potenciales en la aplicación.
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App /> {/* Renderiza la aplicación dentro de StrictMode */}
   </StrictMode>,
 )
