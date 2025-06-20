260fdc12762c4636736a1f0a119079911386757e
jesualujan <lujan.oficial@gmail.com>
2025-06-19 19:50:19 -0600

    ✨ import del componente y mostrar u ocultar

--- 02.Life_Cycling_Component/src/App.jsx
+++ 02.Life_Cycling_Component/src/App.jsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
+import GuessThePokemon from "./components/GuessThePokemon/GuessThePokemon";
+import "./App.css";
 function App() {
+  const [showGame, setShowGame] = useState(false);
   return (
     <>
-      <h1> Adivina quien es ese pokemon</h1>
+      <div className="text-center mt-4">
+        <button
+          className="btn btn-danger mb3"
+          onClick={() => setShowGame(!showGame)}
+        >
+          {showGame ? "Ocultar juego" : "Mostrar juego"}
+        </button>
+        {showGame && <GuessThePokemon />}
+      </div>
     </>
   );
 }
