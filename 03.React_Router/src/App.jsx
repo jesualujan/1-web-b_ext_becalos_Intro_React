2943dd43d2aec20509378abf36ad37667279768c
jesualujan <lujan.oficial@gmail.com>
2025-06-24 21:28:59 -0600

    ✨ validamos si tenemos un usuario o no

--- 03.React_Router/src/App.jsx
+++ 03.React_Router/src/App.jsx
@@ -1,12 +1,19 @@
+import {useState, useEffect } from 'react';
 import Navbar from "./components/Navbar"
 import AppRoutes from "./routes/AppRoutes"
 function App() {
 
+   const[user, setUser] = useState (null);
+
+   useEffect(() => {
+    const userData = JSON.parse(localStorage.getItem('user'));
+    if(userData) setUser(userData)
+   }, [])
 
   return (
     <>
-     <Navbar />
-     <AppRoutes />
+     <Navbar user={user} setUser={setUser} />
+     <AppRoutes user={user} setUser={setUser} />
     </>
   )
 }
