0288e5442e0d564b6282834d0b454f46e3f700e9
jesualujan <lujan.oficial@gmail.com>
2025-06-24 21:05:35 -0600

    ✨ import de BrowserRouter

--- 03.React_Router/src/main.jsx
+++ 03.React_Router/src/main.jsx
@@ -1,11 +1,20 @@
+// NODEJS DEPENDENCIES
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+// STYLES 
 import './index.css'
+// TAILWIND STYLES
 import './styles/index.css'
+// COMPONENTS
 import App from './App.jsx'
+// LIBRERIAS (REACT ROUTER DOM)
+import { BrowserRouter } from 'react-router-dom'
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <BrowserRouter>
+    <StrictMode>
     <App />
-  </StrictMode>,
+    </StrictMode>,
+  </BrowserRouter>
 )
