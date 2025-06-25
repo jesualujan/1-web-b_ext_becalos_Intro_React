386802b462dd81093f2cdce96817c729adcceed2
jesualujan <lujan.oficial@gmail.com>
2025-06-24 21:28:44 -0600

    ✨ protegemos la ruta home

--- 03.React_Router/src/routes/AppRoutes.jsx
+++ 03.React_Router/src/routes/AppRoutes.jsx
@@ -4,14 +4,15 @@ import {Routes, Route, Navigate } from 'react-router-dom';
 import Login from '../pages/Login';
 import Signup from '../pages/Signup';
 
-const AppRoutes = () => (
+const AppRoutes = ({user, setUser}) => (
     // Routes -> sustituye a swtich
     <Routes>
         <Route path="/" element={
-        <h1> Bienvenido </h1>
+          user ? <h1 className='text-center'>Bienvenido {user.email} </h1>
+          : <Navigate to="/login" />
         } />
-        <Route path="/login" element={Login} />
-        <Route path="/signup" element={Signup} />
+        <Route path="/login" element={<Login setUser={setUser} />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="*" element={
             <h2 className="text-center">404 Not Found</h2>
         } />
